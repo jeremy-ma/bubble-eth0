a5ad23a45ae43f6e4ed01b35f0ad05ee4c965273
jeremy-ma <jeremy_ma@outlook.com>
2015-03-14 10:28:37 +1100

    dmasdf

--- sockettest.py
+++ sockettest.py
@@ -5,6 +5,7 @@ data = {"type": "hello", "team": "bubble"}
 test_exchange = '10.0.39.122'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((test_exchange, 25000))
+print(json.dumps(data))
 s.send(json.dumps(data))
 result = json.loads(s.recv(1024))
 print result
