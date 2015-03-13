65f22f01e896d053be5190a27501ac6ef8586b11
jeremy-ma <jeremy_ma@outlook.com>
2015-03-14 10:43:03 +1100

    dasf

--- sockettest.py
+++ sockettest.py
@@ -6,6 +6,10 @@ test_exchange = '10.0.39.122'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((test_exchange, 25000))
 s.send(json.dumps(data) + '\n')
-result = json.loads(s.recv(1024))
+result = json.loads(s.recv(110000))
 print result
 s.close()
+
+
+
+
