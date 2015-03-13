19ed0d73952c4342bde87f6eb90d4fe5fca9e2ce
jeremy-ma <jeremy_ma@outlook.com>
2015-03-14 10:19:33 +1100

    dum

---
+++ sockettest.py
@@ -0,0 +1,11 @@
+import socket
+import json
+
+data = {"type": "hello", "team": "bubble"}
+test_exchange = '10.0.39.122'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((test_exchange, 25000))
+s.send(json.dumps(data))
+result = json.loads(s.recv(1024))
+print result
+s.close()
